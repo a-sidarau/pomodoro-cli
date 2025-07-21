64ac5352158877b4e9e0a5d13299679fe44c6194
Anton Sidarau <anthony.sidarau@gmail.com>
2025-07-21 11:51:06 +0300

    Add functions to change durations and control input

--- pomodoro_app.py
+++ pomodoro_app.py
@@ -13,7 +13,7 @@ TODO:
 6) Show notifications
 
 ver1:
-1) Show timer with with defaults
+1) Show timer with  defaults
 2) Start timer
 3) Stop timer
 4) Start over
@@ -24,10 +24,58 @@ def clear_console():
     # clearing the console
     os.system('clear')
 
+
 def show_notification():
     pass
 
-def timer(work_minutes=40, short_break_minutes=10, long_break_minutes=20, cycles=3):
+
+def countdown(minutes: int, session: str):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        sys.stdout.write(f"\r{session}: {mins:02d}:{secs:02d}")
+        sys.stdout.flush()
+        time.sleep(1)
+        seconds -= 1
+
+
+def change_cycles_amount() -> int:
+    # procedure to change the amount of work cycles
+    while True:
+        try:
+            cycles = int(input("Set work cycles amount: "))
+        except Exception:
+            print("Error! Input a number")
+        
+        # check if number is more than 0
+        # if not - start over
+        if cycles > 0:
+            return cycles
+        else:
+            print("Enter number > 0")
+            continue
+
+
+def change_sessions_duration(work_minutes: int, short_break_minutes: int,
+                             long_break_minutes: int):
+    while True:
+        try:
+             work_minutes = int(input("Enter work session duration: "))
+             short_break_minutes = int(input("Enter short break duration: "))
+             long_break_minutes = int(input("Enter long break duration: "))
+        except Exception:
+             print("Error! Input a number")
+        continue
+    
+
+def timer_main():
+    # main function for timer app
+    # setting default values for main variables
+    work_minutes = 40
+    short_break_minutes = 10
+    long_break_minutes = 20
+    cycles = 3
+    
     while True:
         print("Pomodoro CLI timer")
         print("------------------")
@@ -35,18 +83,37 @@ def timer(work_minutes=40, short_break_minutes=10, long_break_minutes=20, cycles
         print(f"Short break: {short_break_minutes} min" )
         print(f"Long break (after all cycles): {long_break_minutes} min")
         print(f"Cycles: {cycles}")
-        print("------------------")
-        print("Actions:")
-        print("1) Start work session")
-        print("2) Change sessions duration")
-        print("3) Change cycles amount")
-        print("0) Exit app ")
-        print("------------------")
-        choose = input("Choose action: ")
-        match choose:
-            case "0":
-                sys.exit()
+
+        while True:
+            print("------------------")
+            print("Actions:")
+            print("1) Start work session")
+            print("2) Change sessions duration")
+            print("3) Change cycles amount")
+            print("0) Exit app ")
+            print("------------------")
+            
+            # Checking for input to be an int number 
+            # Starting over if input is not a number
+            try:
+                choose = int(input("Enter number of action: "))
+            except Exception:
+                print("Error! Input correct number")
+                continue
+            
+            match choose:
+                case "1":
+                    pass
+                    # countdown(work_minutes, "Work Session")
+                case "2":
+                    change_sessions_duration(work_minutes, short_break_minutes, long_break_minutes)
+                case "3":
+                    cycles = change_cycles_amount()
+                case "0":
+                    sys.exit()
+                case _:
+                    print("No action with this number. Enter valid number from the list")
 
 
 if __name__ == "__main__":
-    timer()
+    timer_main()
